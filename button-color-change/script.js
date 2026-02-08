d51010d36c702580a0071c48f9bb99dec9973ef5
Murat Karabiyik <muratkarabiyik@hotmail.co.uk>
2026-02-08 19:14:21 +0000

    Initial commit for button color change practice

---
+++ button-color-change/script.js
@@ -0,0 +1,8 @@
+// Button change color on click
+const button = document.getElementById("ColorButton");
+
+button.addEventListener("click", function() {
+    button.style.backgroundColor = "rgb(167,252,11)";
+});
+
+
